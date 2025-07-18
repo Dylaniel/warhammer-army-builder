6c792c5c52c9a488bf470379e592c214e13495ab
Dylaniel <dylan@shanaghy.com>
2025-07-17 18:42:50 -0700

    fixed major tailwind issue that resolved styles not being applied

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"ZynLT08HVMJLkFw565lDZ0weN0eUuWULdLFXB6CuPtk=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
